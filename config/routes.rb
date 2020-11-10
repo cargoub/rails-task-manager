3ecd18abf89db0c6bbf04211d4351a1a2fbe5494
Chafik Argoub <cargoub@gmail.com>
2020-11-10 15:55:33 +0100

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # see all the tasks
   get "/tasks", to: "tasks#index"
+  # show all the tasks
+  get "/tasks/:id", to: "tasks#show", as: "task"
 end
